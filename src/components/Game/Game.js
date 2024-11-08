28e0709f5c379f2d787f7029f67c9dc42fa83c12
1kevinson <kevinkouomeu@icloud.com>
2024-11-08 17:35:35 +0100

    🚀 Simplify toast import in Game component
    - Remove unused `toast` import
    - Maintain `Toaster` import from `react-hot-toast`
    - Cleanup improves code readability
    
    Removed the unused `toast` import to streamline the Game component and enhance code clarity. Keeping the `Toaster` import ensures no functionality is lost.

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -4,7 +4,7 @@ import {sample} from '../../utils';
 import {WORDS} from '../../data';
 import WordInput from "../WordInput";
 import Guesses from "../Guesses";
-import toast, {Toaster} from "react-hot-toast";
+import {Toaster} from "react-hot-toast";
 
 // Pick a random word on every page load.
 const answer = sample(WORDS);
